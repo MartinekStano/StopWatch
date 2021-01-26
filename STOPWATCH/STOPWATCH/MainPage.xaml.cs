b57435e9a02de5423f6d8892d1668de70c56d11d
Martin Staňo <martinmatostano@gmail.com>
2021-01-26 18:05:12 +0100

    Add project files.

---
+++ STOPWATCH/STOPWATCH/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+namespace STOPWATCH
+{
+    public partial class MainPage : ContentPage
+    {
+        Stopwatch stopWatch;
+        public MainPage()
+        {
+            InitializeComponent();
+            stopWatch = new Stopwatch();
+
+            lblStopWatch.Text = "00:00:00:0000";
+        }
+
+        private void buttonStart_Clicked(object sender, EventArgs e)
+        {
+            if(!stopWatch.IsRunning)
+            { 
+            stopWatch.Start();
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(100), () =>
+            {
+                lblStopWatch.Text = stopWatch.Elapsed.ToString();
+                if(!stopWatch.IsRunning)
+                { 
+                return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            );
+            }
+        }
+
+        private void buttonReset_Clicked(object sender, EventArgs e)
+        {
+            lblStopWatch.Text = "00:00:00:0000";
+            buttonStart.Text = "Start";
+            stopWatch.Reset();
+
+        }
+
+        private void buttonStop_Clicked(object sender, EventArgs e )
+        {
+            buttonStart.Text = "Resume";
+            stopWatch.Stop();
+        }
+    }
+}
